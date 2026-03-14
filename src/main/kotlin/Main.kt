b5515285c93452d3976591fc05a9750d67bb5fa0
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2026-03-14 04:54:15 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+@file:Suppress("unused")
+
+package com.harukadev
+
+// A higher-level variable
+val author: String = "Albert"
+
+fun main() {
+    println("Hello World!")
+    val popcorn = 5
+    val dog = "dog"
+    var customers = 5
+    val age = 21.3
+
+    customers += 1
+
+    println(popcorn)
+    println("There are $customers customers")
+    print("hello ") // the two on same line
+    print("world") // the two on same line
+}
